e9ee5d532af8f3d8a4237b40323f3ae91b7aeef1
Floriano Victor Larangeira Peixoto <rotc21@gmail.com>
2015-01-14 00:16:43 -0200

    tabelas e cargas

---
+++ comeerj/src/main/resources/carga equipe.sql
@@ -0,0 +1,41 @@
+/*
+-- Query: SELECT * FROM comeerj.equipe
+LIMIT 0, 1000
+
+-- Date: 2015-01-14 00:14
+*/
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (10,'Comissao de Alimentacao');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (11,'Comissao de Alojamento');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (12,'Comissao de Divulgacao');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (13,'Comissao de Estudos/Arte');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (14,'Comissao de Multimeios');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (15,'Comissao de Pequenos Companheiros');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (16,'Comissao de Recepcao');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (17,'Comissao de Servicos Gerais');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (18,'Comissao Medica, Atend. Fraterno e Passe');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (19,'Coordenacao Geral/Espelho da area Administrativa');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (20,'Coordenacao Geral/Espelho da area de Apoio Logistico');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (21,'Coordenacao Geral/Espelho da area de Estudos Doutrinarios');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (22,'Coordenacao Geral/Espelho de area de Assist. Medica e Atend. Fraterno');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (23,'Coordenacao Geral/Espelho de area de Assist. Medica e Atend. Fraterno');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (24,'Coordenacao Geral/Espelho de area de Assist. Medica e Atend. Fraterno');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (25,'Comissao de Alimentacao');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (26,'Comissao de Alojamento');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (27,'Comissao de Divulgacao');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (28,'Comissao de Estudos/Arte');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (29,'Comissao de Multimeios');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (30,'Comissao de Pequenos Companheiros');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (31,'Comissao de Recepcao');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (32,'Comissao de Servicos Gerais');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (33,'Comissao Medica, Atend. Fraterno e Passe');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (34,'Coordenacao Geral/Espelho da area Administrativa');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (35,'Coordenacao Geral/Espelho da area de Apoio Logistico');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (36,'Coordenacao Geral/Espelho da area de Estudos Doutrinarios');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (37,'Coordenacao Geral/Espelho de area de Assist. Medica e Atend. Fraterno');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (38,'Colheita');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (39,'Flores');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (40,'Pequenos Companheiros');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (41,'Polem');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (42,'Sementes');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (43,'Tarefeiro');
+INSERT INTO `equipe` (`idEquipe`,`nome`) VALUES (44,'Tarefeiros do Bem');
